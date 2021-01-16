9e4b0e706de1a6db77b3b991cdb6636233dfd779
Paul-Jason Mello <mello.pauljason@gmail.com>
2021-01-15 22:46:40 -0800

    Add files via upload
    Unresponsive version of my finalized Website 1.0
    Minor bugs will be squashed and the website will become responsive in time.

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        
+        if (this.scrollY > 50) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    function moveToSelected(element) {
+
+        if (element == "current-right") {
+            var selected = $(".current-view").next();
+        } else if (element == "current-left") {
+            var selected = $(".current-view").prev();
+        } else {
+            var selected = element;
+        }
+
+        var next = $(selected).next();
+        var prev = $(selected).prev();
+        var prevSecond = $(prev).prev();
+        var nextSecond = $(next).next();
+
+        $(selected).removeClass("current-left current-right far-right far-left hide-right-view hide-left-view").addClass("current-view");
+
+
+        $(prev).removeClass().addClass("current-left");
+        $(next).removeClass().addClass("current-right");
+
+        $(nextSecond).removeClass().addClass("far-right");
+        $(prevSecond).removeClass().addClass("far-left");
+
+        $(nextSecond).nextAll().removeClass().addClass("hide-right-view");
+        $(prevSecond).prevAll().removeClass().addClass("hide-left-view");
+
+    }
+
+    $('#carousel div').click(function() {
+        if (this.classList.contains("project-info") || this.classList.contains("project-code") || this.classList.contains("project-name") || this.classList.contains("project-description")) {
+
+        } else {
+            moveToSelected($(this));
+        }
+    });
+    
+});
